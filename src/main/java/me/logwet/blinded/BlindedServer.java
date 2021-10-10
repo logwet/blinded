77c5a143c0d2db29bece907445f3ab2b6558bb00
logwet <logwet@users.noreply.github.com>
2021-10-10 16:50:32 +1100

    Parity with noverworld upstream

--- src/main/java/me/logwet/blinded/BlindedServer.java
+++ src/main/java/me/logwet/blinded/BlindedServer.java
@@ -1,8 +1,10 @@
 package me.logwet.blinded;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
-import org.apache.logging.log4j.Level;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
+@Environment(EnvType.SERVER)
 public class BlindedServer implements DedicatedServerModInitializer {
     @Override
     public void onInitializeServer() {
